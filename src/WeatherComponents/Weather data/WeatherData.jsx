ca80e2dc5e0b456267812018cd6df5dd1a290df6
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-05-23 13:15:31 -0700

    Adjusting the api call

--- src/WeatherComponents/Weather data/WeatherData.jsx
+++ src/WeatherComponents/Weather data/WeatherData.jsx
@@ -7,7 +7,7 @@ const WeatherData = ({weatherData, city}) => {
         {
             weatherData &&(
                 <Description>
-                    <h2>The Weather information {city} is :</h2>
+                    <h2>The Weather update at {weatherData.name} is :</h2>
                     <div>Weather-Icon: <img src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} alt={weatherData.weather[0].description} /></div>
                     <p>Description: {weatherData.weather[0].description}</p>
                     <p>Temperature: {Math.round(weatherData.main.temp - 273.15)} &#8451;</p>
