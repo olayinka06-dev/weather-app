be55901e5beb21a79c6d21660c23c9a5a279da5c
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-05-24 08:07:47 -0700

    Adjusting the preloader page

--- src/WeatherComponents/Preloader page/Preloader.jsx
+++ src/WeatherComponents/Preloader page/Preloader.jsx
@@ -16,13 +16,21 @@ const Preloader = () => {
     </Wrapper>
   )
 }
+const progressAnimation = keyframes`
+  0% {
+    transform: scaleX(0);
+  }
+  100% {
+    transform: scaleX(1);
+  }
+`;
+
 const Wrapper = styled.div`
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
-
 
   .preloader {
     background-color: #fff;
@@ -34,66 +42,88 @@ const Wrapper = styled.div`
     align-items: center;
     justify-content: center;
   }
-  
+
   .preloader-inner {
     text-align: center;
   }
-  
-  .preloader-logo img {
-    width: 100px;
-    border-radius: 50%;
-    height: auto;
-    animation: loadlogo 1s linear infinite;
+
+  .preloader-logo {
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 20px;
   }
-  @keyframes loadlogo {
-    0%{transform: scale(90%);}
-    100%{transform: scale(115%);}
-  }
+
   .preloader-progress {
     width: 100%;
     height: 5px;
     background-color: #000;
     position: relative;
     margin-top: 20px;
+    transform-origin: left;
+    animation: ${progressAnimation} 2s linear infinite;
   }
-  
-  .preloader-progress:before {
-    content: "";
-    width: 100%;
-    height: 100%;
-    background-color: #fff;
-    position: absolute;
-    top: 0;
-    left: 0;
-    animation: progress 2s ease-in-out backwards;
-  }
-  
-  @keyframes progress {
-    0% {
-      width: 100%;
-    }
-    100% {
-      width: 0%;
-    }
-  }
-
-  // .progress-bar {
-  //   width: 200px;
-  //   height: 10px;
-  //   background-color: lightgray;
-  // }
-
-  // .progress-bar:before {
-  //   content: '';
-  //   display: block;
-  //   width: 0;
-  //   height: 100%;
-  //   background-color: blue;
-  //   transition: width 0.3s ease-in-out;
-  // }
-
-  // .loader .progress-bar:before {
-  //   width: 100%;
-  // }
 `;
+
+// const Wrapper = styled.div`
+//   * {
+//     margin: 0;
+//     padding: 0;
+//     box-sizing: border-box;
+//   }
+
+
+//   .preloader {
+//     background-color: #fff;
+//     z-index: 9999;
+//     height: 100vh;
+//     width: 100%;
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//     justify-content: center;
+//   }
+  
+//   .preloader-inner {
+//     text-align: center;
+//   }
+  
+//   .preloader-logo img {
+//     width: 100px;
+//     border-radius: 50%;
+//     height: auto;
+//     animation: loadlogo 1s linear infinite;
+//   }
+//   @keyframes loadlogo {
+//     0%{transform: scale(90%);}
+//     100%{transform: scale(115%);}
+//   }
+//   .preloader-progress {
+//     width: 100%;
+//     height: 5px;
+//     background-color: #000;
+//     position: relative;
+//     margin-top: 20px;
+//   }
+  
+//   .preloader-progress:before {
+//     content: "";
+//     width: 100%;
+//     height: 100%;
+//     background-color: #fff;
+//     position: absolute;
+//     top: 0;
+//     left: 0;
+//     animation: progress 2s ease-in-out backwards;
+//   }
+  
+//   @keyframes progress {
+//     0% {
+//       width: 100%;
+//     }
+//     100% {
+//       width: 0%;
+//     }
+//   }
+
+// `;
 export default Preloader;
