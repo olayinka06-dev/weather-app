63a03e15ebe51e3217eab79347e8759b896a8a69
olayinka06-dev <olayinkaconsult06@gmail.com>
2023-05-24 06:49:32 -0700

    Adjusting the preloader page

--- src/WeatherComponents/Preloader page/Preloader.jsx
+++ src/WeatherComponents/Preloader page/Preloader.jsx
@@ -3,8 +3,16 @@ import styled from 'styled-components';
 
 const Preloader = () => {
   return (
-    <Wrapper className='loader'>
-      <div className="progress-bar" />
+    <Wrapper>
+      <div class="preloader">
+        <div class="preloader-inner">
+          <div class="preloader-logo">
+            Insert your logo or any image here
+            {/* <img src="./images/check-line.png" alt=""/> */}
+          </div>
+          <div class="preloader-progress"></div>
+        </div>
+      </div>
     </Wrapper>
   )
 }
@@ -15,30 +23,77 @@ const Wrapper = styled.div`
     box-sizing: border-box;
   }
 
-  height: 100vh;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
 
-  .progress-bar {
-    width: 200px;
-    height: 10px;
-    background-color: lightgray;
-  }
-
-  .progress-bar:before {
-    content: '';
-    display: block;
-    width: 0;
-    height: 100%;
-    background-color: blue;
-    transition: width 0.3s ease-in-out;
-  }
-
-  .loader .progress-bar:before {
+  .preloader {
+    background-color: #fff;
+    z-index: 9999;
+    height: 100vh;
     width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
   }
+  
+  .preloader-inner {
+    text-align: center;
+  }
+  
+  .preloader-logo img {
+    width: 100px;
+    height: auto;
+  }
+  
+  .preloader-progress {
+    width: 100%;
+    height: 5px;
+    background-color: #000;
+    position: relative;
+    margin-top: 20px;
+  }
+  
+  .preloader-progress:before {
+    content: "";
+    width: 0%;
+    height: 100%;
+    background-color: #fff;
+    position: absolute;
+    top: 0;
+    left: 0;
+    animation: progress 2s ease-in-out forwards;
+  }
+  
+  @keyframes progress {
+    0% {
+      width: 0%;
+    }
+    100% {
+      width: 100%;
+    }
+  }
+
+  .preloader.preload-finish {
+    opacity: 0;
+    visibility: hidden;
+    transition: opacity 0.5s ease-in-out;
+  }
+  // .progress-bar {
+  //   width: 200px;
+  //   height: 10px;
+  //   background-color: lightgray;
+  // }
+
+  // .progress-bar:before {
+  //   content: '';
+  //   display: block;
+  //   width: 0;
+  //   height: 100%;
+  //   background-color: blue;
+  //   transition: width 0.3s ease-in-out;
+  // }
+
+  // .loader .progress-bar:before {
+  //   width: 100%;
+  // }
 `;
 export default Preloader;
